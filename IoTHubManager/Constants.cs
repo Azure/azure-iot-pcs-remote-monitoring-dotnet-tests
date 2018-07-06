550a7457a74464d81f9b2335f7eba3eb875ae1d8
Sushil Raje <suraje@microsoft.com>
2018-07-05 19:05:35 -0700

    Job tests initial commit

---
+++ IoTHubManager/Constants.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IoTHubManager
+{
+    class Constants
+    {
+
+        public const string TEST = "Type";
+        public const string INTEGRATION_TEST = "IntegrationTest";
+
+        public const string IOTHUB_ADDRESS = "http://localhost:9002/v1";
+        public const string DEVICE_PATH = "/devices";
+
+        private const string DEVICES_DIR = "./resources/devices/";
+        public const string DEVICE_FILE_AUTO_GEN_AUTH = DEVICES_DIR + "Device_Template_Auto_Generated_Auth.json",
+               DEVICE_FILE_SYMMETRIC_AUTH = DEVICES_DIR + "Device_Template_Symmetric_Auth.json",
+               DEVICE_FILE_X509_AUTH = DEVICES_DIR + "Device_Template_X509_Auth.json";
+
+
+        public const string DEVICE_SIMULATION_ADDRESS = "http://localhost:9003/v1";
+        public const string SIMULATION_PATH = "/simulations/";
+        public const string SIMULATED_DEVICE = "chiller-01";
+        public const string SIMULATED_FAULTY_DEVICE = "elevator-02";
+
+    }
+}
---
+++ IoTHubManager/CreateJobsTest.cs
@@ -0,0 +1,52 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Security.Cryptography;
+using Helpers.Http;
+using Xunit;
+using Newtonsoft.Json.Linq;
+
+namespace IoTHubManager
+{
+    class CreateJobsTest
+    {
+        private readonly IHttpClient httpClient;
+
+
+        public CreateJobsTest()
+        {
+            this.httpClient = new HttpClient();
+            createSimulatedDevice();
+        }
+
+
+
+
+        private void createSimulatedDevice()
+        {
+            var devices = getSimulatedDevices();
+            var deviceModels = devices["DeviceModels"];
+
+            foreach (JObject device in deviceModels)
+            {
+
+            }
+
+        }
+
+        private JObject getSimulatedDevices()
+        {
+            var request = new HttpRequest(Constants.DEVICE_SIMULATION_ADDRESS + Constants.SIMULATION_PATH);
+            IHttpResponse response = this.httpClient.GetAsync(request).Result;
+            return JObject.Parse(response.Content);
+        }
+
+        private IHttpResponse request(string content)
+        {
+            var request = new HttpRequest(Constants.IOTHUB_ADDRESS + Constants.DEVICE_PATH);
+            request.SetContent(content);
+            return this.httpClient.PostAsync(request).Result;
+        }
+    }
+}
