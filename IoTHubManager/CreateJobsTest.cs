cded1900e01170899816ad957a3d6f4b027a4db4
Sushil Raje <suraje@microsoft.com>
2018-07-05 19:06:56 -0700

    Job tests initial commit

--- IoTHubManager/CreateJobsTest.cs
+++ IoTHubManager/CreateJobsTest.cs
@@ -12,13 +12,17 @@ namespace IoTHubManager
     class CreateJobsTest
     {
         private readonly IHttpClient httpClient;
-
+        private int simulatedDeviceCount = 0;
+        private int simulatedFaultyDeviceCount = 0;
 
         public CreateJobsTest()
         {
             this.httpClient = new HttpClient();
             createSimulatedDevice();
         }
+
+        
+
 
 
 
@@ -30,9 +34,27 @@ namespace IoTHubManager
 
             foreach (JObject device in deviceModels)
             {
-
+                if (device["Id"].ToString() == Constants.SIMULATED_DEVICE)
+                {
+                    simulatedDeviceCount = device["Count"].ToObject<int>();
+                    device["Count"] = (simulatedDeviceCount + 1).ToString();
+                }
+                if (device["Id"].ToString() == Constants.SIMULATED_FAULTY_DEVICE)
+                {
+                    simulatedFaultyDeviceCount = device["Count"].ToObject<int>();
+                    device["Count"] = (simulatedFaultyDeviceCount + 1).ToString();
+                }
             }
+            putSimulatedDevices(devices);
 
+        }
+
+        private void putSimulatedDevices(JObject devices)
+        {
+            var request = new HttpRequest(Constants.DEVICE_SIMULATION_ADDRESS + Constants.SIMULATION_PATH);
+            request.SetContent(devices);
+            IHttpResponse response = this.httpClient.PutAsync(request).Result;
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
         }
 
         private JObject getSimulatedDevices()
