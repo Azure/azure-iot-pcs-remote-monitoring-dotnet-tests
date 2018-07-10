7856f22daef2efebba12681baf1bde029e1bb89a
Sushil Raje <suraje@microsoft.com>
2018-07-09 19:56:13 -0700

    Job Tests

--- IoTHubManager/Constants.cs
+++ IoTHubManager/Constants.cs
@@ -17,26 +17,51 @@ namespace IoTHubManager
             public const string JOBS_PATH = "/jobs/";
 
             public const string DEVICE_SIMULATION_ADDRESS = "http://localhost:9003/v1";
-            public const string SIMULATION_PATH = "/simulations/";
+            public const string SIMULATION_PATH = "/simulations/1";
         }
 
-        public class PATH
+        public class Path
         {
             private const string DEVICES_DIR = "./resources/devices/";
-            private const string TAGS_DIR = "./resources/tags/";
+            private const string JOBS_DIR = "./resources/jobs/";
 
             public const string
                 DEVICE_FILE_AUTO_GEN_AUTH = DEVICES_DIR + "Device_Template_Auto_Generated_Auth.json",
                 DEVICE_FILE_SYMMETRIC_AUTH = DEVICES_DIR + "Device_Template_Symmetric_Auth.json",
-                DEVICE_FILE_X509_AUTH = DEVICES_DIR + "Device_Template_X509_Auth.json",
-                TAGS_FILE = TAGS_DIR + "Tags.json";
+                DEVICE_FILE_X509_AUTH = DEVICES_DIR + "Device_Template_X509_Auth.json";
+
+            public const string
+                TAGS_FILE = JOBS_DIR + "Tags.json",
+                REBOOT_METHOD_FILE = JOBS_DIR + "Method_Chiller_Reboot.json",
+                FIRMWAREUPDATE_METHOD_FILE = JOBS_DIR + "Method_Elevator_FrimwareUpdate.json",
+                SIMULATION_FILE = JOBS_DIR + "Simulation.json";
+
         }
 
+        public class TemplateKeys
+        {
+            public const string DEVICE_ID = "{DeviceId}";
+            public const string PRIMARY_KEY = "{PrimaryKey}";
+            public const string SECONDARY_KEY = "{SecondaryKey}";
+            public const string PRIMARY_TH = "{PrimaryThumbprint}";
+            public const string SECONDARY_TH = "{SecondaryThumbprint}";
 
+            public const string JOB_ID = "{JobId}";
+            public const string FAULTY_DEVICE_ID = "{FaultyDeviceId}";
+        }
 
+        public class SimulatedDevices
+        {
+            public const string SIMULATED_DEVICE = "chiller-01";
+            public const string SIMULATED_FAULTY_DEVICE = "elevator-02";
+        }
 
-        public const string SIMULATED_DEVICE = "chiller-01";
-        public const string SIMULATED_FAULTY_DEVICE = "elevator-02";
+        public class Jobs
+        {
+            public const int WAIT = 600;
+            public const int MAX_TRIALS = 5;
+        }
+
 
     }
 }
--- IoTHubManager/CreateJobsTest.cs
+++ IoTHubManager/CreateJobsTest.cs
@@ -1,5 +1,5 @@
 ﻿ using System;
-using System.Collections.Generic;
+using System.Threading;
 using System.Text;
 using System.Net;
 using System.Security.Cryptography;
@@ -12,52 +12,117 @@ namespace IoTHubManager
     public class CreateJobsTest
     {
         private readonly IHttpClient httpClient;
+
         private readonly string simulatedDeviceId;
         private readonly string simulatedFaultyDeviceId;
 
         public CreateJobsTest()
         {
             this.httpClient = new HttpClient();
-            SimulatedDevices simulatedDevices = new SimulatedDevices();
-            simulatedDeviceId = Constants.SIMULATED_DEVICE + "." + simulatedDevices.healthyDeviceNo.ToString();
-            simulatedFaultyDeviceId = Constants.SIMULATED_FAULTY_DEVICE + "." + simulatedDevices.faultyDeviceNo.ToString();
+            Simulation simulation = Simulation.GetSimulation();
+
+            simulatedDeviceId = Constants.SimulatedDevices.SIMULATED_DEVICE + "." + simulation.healthyDeviceNo.ToString();
+            simulatedFaultyDeviceId = Constants.SimulatedDevices.SIMULATED_FAULTY_DEVICE + "." + simulation.faultyDeviceNo.ToString();
         }
 
         [Fact, Trait(Constants.TEST, Constants.INTEGRATION_TEST)]
-        public void Creates_Tags_Failson_InCorrectORMissingTags()
+        public void Creates_Tags_On_Simulated_Devices()
         {
+            
             var tagJobResponse = CreateTags();
             Assert.Equal(HttpStatusCode.OK, tagJobResponse.StatusCode);
 
             var tagJob = JObject.Parse(tagJobResponse.Content);
-            Assert.Equal<int>(4, tagJob["status"].ToObject<int>());
-            Assert.Equal<int>(7, tagJob["Type"].ToObject<int>());
+            Assert.Equal<int>(7, tagJob["Status"].ToObject<int>());
+            Assert.Equal<int>(4, tagJob["Type"].ToObject<int>());
 
-            var tagJobStatus = getTagJobStatus(tagJob["JobId"].ToString());
-            Assert.Equal<int>(4, tagJob["status"].ToObject<int>());
-            Assert.Equal<int>(3, tagJob["Type"].ToObject<int>());
-            Assert.Equal<int>(2, tagJob["ResultStatistics"]["DeviceCount"].ToObject<int>());
+            var tagJobStatus = GetJobStatus(tagJob["JobId"].ToString());
+
+            for (int trials = 0;  trials < Constants.Jobs.MAX_TRIALS; trials++ )
+            {
+                if (3 == tagJobStatus["Status"].ToObject<int>())
+                {
+                    break;
+                }
+                Thread.Sleep(Constants.Jobs.WAIT);
+                tagJobStatus = GetJobStatus(tagJob["JobId"].ToString());
+            }
+
+            Assert.Equal<int>(3, tagJobStatus["Status"].ToObject<int>());   //Assert to see if Last try yielded correct status.
+            Assert.Equal<int>(4, tagJobStatus["Type"].ToObject<int>());
+
         }
 
-        private JObject getTagJobStatus(string JobId)
+        [Fact, Trait(Constants.TEST, Constants.INTEGRATION_TEST)]
+        public void Run_Method_On_Simulated_Devices()
+        {
+            TestRunMethod(Constants.Path.REBOOT_METHOD_FILE);               //Method with NO Args
+            TestRunMethod(Constants.Path.FIRMWAREUPDATE_METHOD_FILE);       //Method with Args
+        }
+
+        private void TestRunMethod(string methodFile)
+        {
+            var methodJobResp = RunMethods(methodFile);
+            Assert.Equal(HttpStatusCode.OK, methodJobResp.StatusCode);
+
+            var methodJob = JObject.Parse(methodJobResp.Content);
+            Assert.Equal<int>(7, methodJob["Status"].ToObject<int>());
+            Assert.Equal<int>(3, methodJob["Type"].ToObject<int>());
+
+            var methodJobStatus = ReTry_GetJobStatus(methodJob["JobId"].ToString());
+
+            Assert.Equal<int>(3, methodJobStatus["Status"].ToObject<int>()); //Assert to see if Last try yielded correct status.
+            Assert.Equal<int>(3, methodJobStatus["Type"].ToObject<int>());
+
+        }
+
+
+        private IHttpResponse CreateTags()
+        {
+            var TAGS = System.IO.File.ReadAllText(Constants.Path.TAGS_FILE);
+            string jobId = Guid.NewGuid().ToString();
+
+            TAGS = TAGS.Replace(Constants.TemplateKeys.JOB_ID, jobId)
+                       .Replace(Constants.TemplateKeys.DEVICE_ID, simulatedDeviceId)
+                       .Replace(Constants.TemplateKeys.FAULTY_DEVICE_ID, simulatedFaultyDeviceId);
+            return Request(TAGS);
+        }
+
+        private IHttpResponse RunMethods(string methodFile)
+        {
+            var METHODS = System.IO.File.ReadAllText(methodFile);
+            string jobId = Guid.NewGuid().ToString();
+            
+            METHODS = METHODS.Replace(Constants.TemplateKeys.JOB_ID, jobId)
+                             .Replace(Constants.TemplateKeys.DEVICE_ID, simulatedDeviceId);
+            return Request(METHODS);
+        }
+
+        private JObject GetJobStatus(string JobId)
         {
             IHttpResponse jobStatusResponse = Request(JobId, null);
             Assert.Equal(HttpStatusCode.OK, jobStatusResponse.StatusCode);
             return JObject.Parse(jobStatusResponse.Content);
         }
 
-        private IHttpResponse CreateTags()
+        private JObject ReTry_GetJobStatus(string jobId)
         {
-            var TAGS = System.IO.File.ReadAllText(Constants.PATH.TAGS_FILE);
-            string jobId = Guid.NewGuid().ToString();
+            var jobStatus = GetJobStatus(jobId);
 
-            TAGS = TAGS.Replace("{JobId}", jobId)
-                       .Replace("{DeviceId}", simulatedDeviceId)
-                       .Replace("{FaultyDeviceId}", simulatedFaultyDeviceId);
-            return Request(TAGS);
+            for (int trials = 0; trials < Constants.Jobs.MAX_TRIALS; trials++)
+            {
+                if (3 == jobStatus["Status"].ToObject<int>())
+                {
+                    break;
+                }
+                Thread.Sleep(Constants.Jobs.WAIT);
+                jobStatus = GetJobStatus(jobId);
+            }
+
+            return jobStatus;
         }
 
-       
+
 
         private IHttpResponse Request(string content)
         {
