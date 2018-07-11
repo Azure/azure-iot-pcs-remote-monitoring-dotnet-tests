8a792fed98dfa1dd2043c5e647128fae158471b7
Sushil Raje <suraje@microsoft.com>
2018-07-10 17:03:02 -0700

    Refactoring the code

--- IoTHubManager/HttpRequestWrapper.cs
+++ IoTHubManager/HttpRequestWrapper.cs
@@ -11,12 +11,16 @@ namespace IoTHubManager
         private readonly IHttpClient httpClient;
         private string uri;
 
-        internal HttpRequestWrapper(string host,string path)
+        internal HttpRequestWrapper(string host, string path)
         {
             httpClient = new HttpClient();
             if (!String.IsNullOrEmpty(host))
             {
-                this.uri = host + path;
+                this.uri = host;
+            }
+            if (!String.IsNullOrEmpty(path))
+            {
+                this.uri += path;
             }
         }
 
@@ -26,16 +30,19 @@ namespace IoTHubManager
             return this.httpClient.GetAsync(request).Result;
         }
 
+        //TODO: Method should accept Map of query parameters and build the query
         internal IHttpResponse Get(string path, string query)
         {
+            string uri = this.uri;
             if (!String.IsNullOrEmpty(path))
             {
                 uri += path;
             }
             if (!String.IsNullOrEmpty(query))
             {
-                uri += query;
+                uri += "?"+query;
             }
+
             var request = new HttpRequest(uri);
             return this.httpClient.GetAsync(request).Result;
         }
@@ -45,13 +52,14 @@ namespace IoTHubManager
             var request = new HttpRequest(uri);
             request.SetContent(content);
             return this.httpClient.PostAsync(request).Result;
+           
         }
 
         internal IHttpResponse Put(JObject content)
         {
             var request = new HttpRequest(uri);
             request.SetContent(content);
-            return this.httpClient.PostAsync(request).Result;
+            return this.httpClient.PutAsync(request).Result;
         }
     }
 }
