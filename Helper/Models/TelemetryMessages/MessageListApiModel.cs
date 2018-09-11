406a8d52483aec29f026a10f3b9509b5df58e2a7
Jill Bender <jbend@microsoft.com>
2018-09-11 16:02:44 -0700

    Basic integration test for Telemetry GET messages with retry logic (#47)
    Add get messages integration test

---
+++ Helper/Models/TelemetryMessages/MessageListApiModel.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Helpers.Models.TelemetryMessages
+{
+    public class MessageListApiModel
+    {
+        private readonly List<MessageApiModel> items = new List<MessageApiModel>();
+        private readonly List<string> properties = new List<string>();
+
+        [JsonProperty(PropertyName = "Items")]
+        public List<MessageApiModel> Items
+        {
+            get { return this.items; }
+        }
+
+        [JsonProperty(PropertyName = "Properties")]
+        public List<string> Properties
+        {
+            get { return this.properties; }
+        }
+
+        [JsonProperty(PropertyName = "$metadata", Order = 1000)]
+        public IDictionary<string, string> Metadata => new Dictionary<string, string>
+        {
+            { "$type", "MessageList;1" },
+            { "$uri", "/v1/messages" },
+        };
+
+        public MessageListApiModel() { }
+    }
+}
