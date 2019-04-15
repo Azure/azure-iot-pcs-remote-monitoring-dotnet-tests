cbb4d8cf1d0110bad15386967097d420c838e1e9
sushilraje <39531904+sushilraje@users.noreply.github.com>
2019-04-15 12:53:21 -0700

    Update devicesimulation.sh

--- scripts/devicesimulation.sh
+++ scripts/devicesimulation.sh
@@ -5,7 +5,7 @@ set -e
 
 DOCKER_ACCOUNT=azureiotpcs
 
-DOCKER_IMAGE="$DOCKER_ACCOUNT/device-simulation-dotnet:DS-1.0.3"
+DOCKER_IMAGE="$DOCKER_ACCOUNT/device-simulation-dotnet:DS-1.0.5"
 DOCKER_PORT=9003
 DOCKER_NAME="device-simulation"
 DOCKER_NETWOK="integrationtests"
